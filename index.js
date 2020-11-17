f864f4d0e0030b0d391f33cf9f725c34166a7435
Pranav Patel <pranavbhai.patel11@gmail.com>
2020-11-16 20:01:05 -0500

    Remove temp index.js file

--- index.js
+++
@@ -1,5 +0,0 @@
-var requirejs = require('requirejs');
-const AugmentTransitionSystem = requirejs("./src/ModelTransformation/augmentTransitionSystem.js");
-const augment = new AugmentTransitionSystem;
-
-console.log(augment);
