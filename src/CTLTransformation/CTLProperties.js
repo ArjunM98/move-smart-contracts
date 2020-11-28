33b14e21412333caf1abaf60b8ca778f9d7f6132
Rohit <rohit.arora1@ibm.com>
2020-11-28 17:35:57 -0500

    Adding parseProperties function and tests associated with it

--- src/CTLTransformation/CTLProperties.js
+++ src/CTLTransformation/CTLProperties.js
@@ -5,6 +5,35 @@ define([], function () {
     var CTLProperties = function () {}
     CTLProperties.prototype.constructor = CTLProperties;
 
+    CTLProperties.prototype.parseProperties = function(model, properties) {
+        var parsedProperties = [];
+        for (const property of properties.split(";")) {
+            var clauses = [];
+            for (const clause of property.split("#")) {
+                var actions = [];
+                for (const action of clause.split("|")) {
+                    var actionName = action.replace(/\s/g,"");
+                    var transitions = [];
+                    for (const transition of model["transitions"]) {
+                        if (transition['actionName'] != undefined && transition['actionName'].replace(/[;\s]+/g,"") === actionName) {
+                            transitions.push(transition['actionName']);
+                        }
+                    }
+                    if (transitions.length != 1) { // action specification is ambiguous since multiple transitions match it
+                        if(transitions.length == 0){
+                            throw "Could not find action: " + action + " Possible reason: Statement was specified without function name.";
+                        }
+                        throw "Ambiguous action (multiple instances occured): " + action;
+                    }
+                    actions.push(transitions[0]); // single transition matches the action specification
+                }
+                clauses.push(actions); // push this clause
+            }
+            parsedProperties.push(clauses); // push this property
+        }
+        return parsedProperties;
+    };
+
     // Generate template that confirms <action> cannot be reached after <action>
     CTLProperties.prototype.generateFirstTemplateProperties = function (bipTransitionsToSMVNames, actionNamesToTransitionNames, properties) {
         var property, clause, propertiesSMV = '';
@@ -58,6 +87,9 @@ define([], function () {
     };
 
     // Generate template that confirms <action> can only occur after <action>
+    // TODO: check why the method of verification is a little different than the whitepaper
+    // Looking further can see U is for until while W is for Weak until
+    // Essentially does the same thing but this is a stronger check
     CTLProperties.prototype.generateSecondTemplateProperties = function (bipTransitionsToSMVNames, actionNamesToTransitionNames, properties) {
         var property, clause, propertiesSMV = '';
         for (property of properties){
