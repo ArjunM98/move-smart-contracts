f64beaa977a5496761fcc3526b4b73ddbf92990f
Rohit <arrohitarora@gmail.com>
2020-12-30 20:59:04 -0500

    Remove comment and add new lines at end of json files

--- app.js
+++ app.js
@@ -9,6 +9,4 @@ const webgme = require('webgme')
 webgme.addToRequireJsPaths(gmeConfig)
 
 const myServer = new webgme.standaloneServer(gmeConfig)
-myServer.start(function () {
-  // console.log('server up');
-})
+myServer.start()
