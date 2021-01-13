9594107a196a4f5a2fbbfcbce2eee96f99432606
Pranav Patel <pranavbhai.patel11@gmail.com>
2021-01-12 19:56:44 -0500

    [BUG] Verification Output File wrong text for first CTL Property (#54)
    * String update for templateOne

--- src/CTLTransformation/CTLProperties.js
+++ src/CTLTransformation/CTLProperties.js
@@ -2,7 +2,7 @@ define([], function () {
   'use strict'
 
   // Default constructor
-  const CTLProperties = function () {}
+  const CTLProperties = function () { }
   CTLProperties.prototype.constructor = CTLProperties
 
   // Generate template that confirms <action> cannot be reached after <action>
@@ -47,7 +47,7 @@ define([], function () {
         propertiesSMV += clause + '|'
       }
       propertiesSMV = propertiesSMV.slice(0, -1)
-      propertiesSMV += ') can happen only after ('
+      propertiesSMV += ') cannot happen after ('
       for (clause of property[1]) {
         propertiesSMV += clause + '|'
       }
